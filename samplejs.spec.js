3f89827f70bd75f35c70738bba61acb5fe3188cf
Simon McEnlly <simon.mcenlly@gmail.com>
2023-05-05 08:45:51 +1000

    Initial commit

---
+++ samplejs.spec.js
@@ -0,0 +1,6 @@
+import { it } from 'vitest';
+
+it('test js test explorer', () => {
+  const wut = 'ok'; wut = 'new ok'
+  console.log('foo');
+})
---
+++ samplets.spec.ts
@@ -0,0 +1,6 @@
+import { it } from 'vitest';
+
+it('test ts test explorer', () => {
+  const wut = 'ok'; wut = 'new ok'
+  console.log('foo');
+})
